5b5c71d7c83f71667535d5ab2d5f2cf4b7874530
slimeball <admin@dopeshit.net>
2017-08-06 22:59:58 -0700

    Update GridcoinAutoNode.sh
    Updated node list from wiki

--- GridcoinAutoNode.sh
+++ GridcoinAutoNode.sh
@@ -1,91 +1,83 @@
 #!/bin/bash
-echo "########### The server will reboot when the script is complete"
-echo "########### Changing to /home/ dir"
+echo "-=-=-=- The server will reboot when the script is complete -=-=-=-"
+echo "-=-=-=- Changing to home dir -=-=-=-"
 cd ~
+echo "-=-=-=- Updating Debian -=-=-=-"
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository -y ppa:gridcoin/gridcoin-daily
+sources="/etc/apt/sources.list.d/gridcoin-gridcoin-daily-jessie.list"
+echo 'deb http://ppa.launchpad.net/gridcoin/gridcoin-stable/ubuntu trusty main' | sudo tee $sources
+echo 'deb-src http://ppa.launchpad.net/gridcoin/gridcoin-stable/ubuntu trusty main' | sudo tee -a $sources
+wget https://mirrors.kernel.org/ubuntu/pool/main/m/miniupnpc/libminiupnpc8_1.6-3ubuntu1.2_amd64.deb
+sudo dpkg -i libminiupnpc8_1.6-3ubuntu1.2_amd64.deb
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install Gridcoinresearchd
+sudo apt-get -y install unzip
 
-# echo "########### Disabling IPv6"
-# sed -i '$anet.ipv6.conf.all.disable_ipv6 = 1' /etc/sysctl.conf
-# sed -i '$anet.ipv6.conf.default.disable_ipv6 = 1' /etc/sysctl.conf
-# sed -i '$anet.ipv6.conf.lo.disable_ipv6 = 1' /etc/sysctl.conf
-# sysctl -p
+echo "-=-=-=- Creating Swap -=-=-=-"
+sudo su -c "dd if=/dev/zero of=/swapfile bs=1M count=2048 ; mkswap /swapfile ; swapon /swapfile"
+echo '/swapfile swap swap defaults 0 0' | sudo tee -a /etc/fstab
 
-echo "########### Adding Google DNS"
-sed -i '$a\        dns-nameservers 8.8.8.8 8.8.4.4' /etc/network/interfaces
+echo "-=-=-=- Create Gridcoin User -=-=-=-"
+sudo useradd -m gridcoin
 
-echo "########### Adding Spideroak source"
-echo "38.121.104.79 spideroak.com" >> /etc/hosts
-
-echo "########### Installing necessary packages"
-apt-get -y dist-upgrade
-apt-get -y update && apt-get -y upgrade; apt-get install -y --no-install-recommends apt-utils && apt-get -y install software-properties-common && apt-get -y install sudo && apt-get -y install git && apt-get -y install gcc && apt-get -y install make && apt-get -y install unzip
-add-apt-repository -y ppa:gridcoin/gridcoin-stable
-apt-get -y update && apt-get -y upgrade; apt-get -y install gridcoinresearchd
-
-echo "########### Creating gridcoin user"
-useradd -m gridcoin
-
-echo "########### Creating gridcoinresearch.conf file"
+echo "-=-=-=- Creating config-=-=-=-"
 cd ~gridcoin
 sudo -u gridcoin mkdir .GridcoinResearch
 cd /home/gridcoin/.GridcoinResearch/
-
-echo "########### Downloading and extracting bootstrap.zip"
-sudo -u gridcoin wget https://spideroak.com/share/N4YFAZLQOBSXEMDP/public/d%3A/Gridcoin.Tools/Share/bootstrap.zip
-sudo unzip bootstrap.zip
-
+sudo -u gridcoin wget http://download.gridcoin.us/download/downloadstake/signed/snapshot.zip
+sudo unzip snapshot.zip
 sudo chown -R gridcoin:gridcoin /home/gridcoin/.GridcoinResearch/*
-config="gridcoinresearch.conf"
+config="/home/gridcoin/.GridcoinResearch/gridcoinresearch.conf"
 sudo -u gridcoin touch $config
-echo "server=1" > $config
-echo "daemon=1" >> $config
-echo "rpcport=9332" >> $config
-echo "listen=1" >> $config
-echo "suppressupgrade=false" >> $config
-echo "suppressvoice=true" >> $config
-echo "enablespeech=false" >> $config
-echo "poolmining=false" >> $config
-echo "UpdatingLeaderboard=false" >> $config
-echo "cpumining=false" >> $config
-echo "addnode=139.162.91.223" >> $config
-echo "addnode=139.162.171.24" >> $config
-echo "addnode=grcnode.adf.lu" >> $config
-echo "addnode=grcnode.tahvok.com" >> $config
-echo "addnode=grcnode01.centralus.cloudapp.azure.com" >> $config
-echo "addnode=grcnode02.eastus.cloudapp.azure.com" >> $config
-echo "addnode=grcnode03.eastus.cloudapp.azure.com" >> $config
-echo "addnode=grcnode04.eastus.cloudapp.azure.com" >> $config
-echo "addnode=grcnode05.westus2.cloudapp.azure.com" >> $config
-echo "addnode=gridcoin.asia" >> $config
-echo "addnode=gridcoin.is.dopeshit.net" >> $config
-echo "addnode=ils.gridcoin.co.il" >> $config
-echo "addnode=la.grcnode.co.uk" >> $config
-echo "addnode=london.grcnode.co.uk" >> $config
-echo "addnode=miami.grcnode.co.uk" >> $config
-echo "addnode=node.gridcoin.us" >> $config
-echo "addnode=node.gridcoinapp.xyz" >> $config
-echo "addnode=node1.chick3nman.com" >> $config
-echo "addnode=nuad.de" >> $config
-echo "addnode=oregon.gridcoin.stablenode.net" >> $config
-echo "addnode=quebec.gridcoin.co.il" >> $config
-echo "addnode=seattle.grcnode.deluxe-host.net" >> $config
-echo "addnode=seattle2.gridcoin.stablenode.net" >> $config
-echo "addnode=singapore.grcnode.co.uk" >> $config
-echo "addnode=toronto01.gridcoin.ifoggz-network.xyz" >> $config
-echo "addnode=vancouver01.gridcoin.ifoggz-network.xyz" >> $config
-randUser=`< /dev/random tr -dc A-Za-z0-9 | head -c64`
-randPass=`< /dev/random tr -dc A-Za-z0-9 | head -c64`
-echo "rpcuser=$randUser" >> $config
-echo "rpcpassword=$randPass" >> $config
+echo "server=1" | sudo tee -a $config
+echo "daemon=1" | sudo tee -a $config
+echo "listen=1" | sudo tee -a $config
+echo "addnode=grcnode.adf.lu" | sudo tee -a $config
+echo "addnode=node.tahvok.com" | sudo tee -a $config
+echo "addnode=grcnode01.centralus.cloudapp.azure.com" | sudo tee -a $config
+echo "addnode=grcnode02.eastus.cloudapp.azure.com" | sudo tee -a $config
+echo "addnode=grcnode03.eastus.cloudapp.azure.com" | sudo tee -a $config
+echo "addnode=grcnode04.eastus.cloudapp.azure.com" | sudo tee -a $config
+echo "addnode=grcnode05.eastus.cloudapp.azure.com" | sudo tee -a $config
+echo "addnode=gridcoin.asia" | sudo tee -a $config
+echo "addnode=gridcoin.crypto.fans" | sudo tee -a $config
+echo "addnode=ils.gridcoin.co.il" | sudo tee -a $config
+echo "addnode=la.grcnode.co.uk" | sudo tee -a $config
+echo "addnode=london.grcnode.co.uk" | sudo tee -a $config
+echo "addnode=miami.grcnode.co.uk" | sudo tee -a $config
+echo "addnode=newyork.gridcoin.blacknetserver.com" | sudo tee -a $config
+echo "addnode=node.gridcoin.us" | sudo tee -a $config
+echo "addnode=node1.gridcoin.xyz" | sudo tee -a $config
+echo "addnode=node1.chick3nman.com" | sudo tee -a $config
+echo "addnode=nuad.de" | sudo tee -a $config
+echo "addnode=oregon.gridcoin.stablenode.net" | sudo tee -a $config
+echo "addnode=quebec.gridcoin.co.il" | sudo tee -a $config
+echo "addnode=seattle.grcnode.deluxe-host.net" | sudo tee -a $config
+echo "addnode=seattle2.gridcoin.stablenode.net" | sudo tee -a $config
+echo "addnode=singapore.grcnode.co.uk" | sudo tee -a $config
+echo "addnode=toronto01.gridcoin.ifoggz-network.xzy" | sudo tee -a $config
+echo "addnode=vancouver01.gridcoin.ifoggz-network.xzy" | sudo tee -a $config
+echo "addnode=gridcoin.hopto.org" | sudo tee -a $config
+echo "rpcport=9332" | sudo tee -a $config
+randUser=`< /dev/urandom tr -dc A-Za-z0-9 | head -c128`
+randPass=`< /dev/urandom tr -dc A-Za-z0-9 | head -c128`
+echo "rpcuser=$randUser" | sudo tee -a $config
+echo "rpcpassword=$randPass" | sudo tee -a $config
 
-echo "########### Setting up chrony"
-cd ~
-git clone git://git.tuxfamily.org/gitroot/chrony/chrony.git
-cd ~/chrony
-./configure
+echo "-=-=-=- Setting up NTP -=-=-=-"
+sudo apt-get -y install ntp
+/etc/init.d/ntp start
 
-echo "########### Cleaning up unnecessary packages"
-apt-get -y remove git && apt-get -y remove gcc && apt-get -y remove make && apt-get -y remove unzip
-apt-get -y autoremove
+echo "-=-=-=- Setting up autostart on boot -=-=-=-"
+sudo echo '#!/bin/sh' | sudo tee /etc/init.d/grcboot.sh
+sudo echo 'sudo -u gridcoin gridcoinresearchd -datadir=/home/gridcoin/.GridcoinResearch/' | sudo tee -a /etc/init.d/grcboot.sh
+sudo echo 'exit 0' | sudo tee -a /etc/init.d/grcboot.sh
+sudo chmod 755 /etc/init.d/grcboot.sh
+sudo update-rc.d grcboot.sh defaults
 
-echo "########### All done! Thank you for supporting the Gridcoin network! Server will now start."
-su - gridcoin -c gridcoinresearchd
+echo "-=-=-=- Add an alias -=-=-=-"
+echo "alias grc='sudo -u gridcoin gridcoinresearchd -datadir=/home/gridcoin/.GridcoinResearch/'" | sudo tee -a ~/.bashrc
+
+sudo reboot +5
